99bebc42fd98576f35f85d824cba43ea7bea4e6c
sworrrnimaS <077bct046.swornima@sagarmatha.edu.np>
2024-07-02 19:24:35 +0545

    added gsap animation

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 ### Responsive Christmas Website
 
 - Responsive Christmas Website Design Using HTML CSS & JavaScript
@@ -9,4 +8,6 @@
 - Developed first with the Mobile First methodology, then for desktop.
 - Compatible with all mobile devices and with a beautiful and pleasant user interface.
 
-
+##Animations used:
+Parallax: https://dixonandmoe.com/rellax/
+GSAP:
--- assets/js/main.js
+++ assets/js/main.js
@@ -75,5 +75,13 @@ window.addEventListener("scroll", scrollActive);
 let parallax = new Rellax(".parallax");
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__village", 1.2, { opacity: 0, y: 100, delay: 0.1 });
+gsap.from(".home__pine", 1.2, { opacity: 0, y: 150, delay: 0.3 });
+gsap.from(".home__mountain-2", 1.2, { opacity: 0, x: 150, delay: 0.5 });
+gsap.from(".home__mountain-3", 1.2, { opacity: 0, x: -150, delay: 0.6 });
+gsap.from(".home__mountain-1", 1.2, { opacity: 0, y: 250, delay: 0.7 });
+gsap.from(".home__moon", 1.2, { opacity: 0, y: 200, delay: 0.8 });
+gsap.from(".home__trineo", 1.2, { opacity: 0, x: -200, delay: 0.8 });
+gsap.from(".home__title", 1.2, { opacity: 0, y: -60, delay: 1 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
